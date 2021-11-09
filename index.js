8fb307816c2bf674b0ed719b06002376942a0029
dvbtrung2302 <baotrung23022000@gmail.com>
2021-11-09 14:56:19 +0700

    change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const axios = require('axios');
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.get('/finance/:code', async (req, res) => {
   try {
